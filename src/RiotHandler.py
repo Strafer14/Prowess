af0d0f81c7d75b69b6a35ac3d44163ede18756ac
Michael Ostrovsky <micha.ostrov@gmail.com>
2020-10-20 22:47:27 +0300

    Removing irrelevant tests

--- src/RiotHandler.py
+++ src/RiotHandler.py
@@ -1,6 +1,8 @@
 import requests
 from os import environ
 
+def make_request(url):
+    return requests.get(url, headers={'X-Riot-Token': self.token})
 
 class RiotHandler:
     def __init__(self):
@@ -13,12 +15,16 @@ class RiotHandler:
     def get_puuid(self, game_name, tag_line):
         url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{}/{}'.format(
             game_name, tag_line)
-        return requests.get(url, headers={'X-Riot-Token': self.token})
+        return make_request(url)
 
-    def get_data(self, region, puuid):
+    def get_matches_list(self, region, puuid):
         url = 'https://{}.api.riotgames.com/val/match/v1/matchlists/by-puuid/{}'.format(
             region, puuid)
-        return requests.get(url, headers={'X-Riot-Token': self.token})
+        return make_request(url)
+
+    def get_match_data(self, match_id):
+        url = ''
+        return make_request(url)
 
 # {
 #     "puuid": "EE8A-dek_wW2K9vwp7SrtdVq8GZ7glvOtKnLEL5gcO6HsOpQoFnlr2F7UMS4Nk7rO1cz-JkvaZ36YQ",
