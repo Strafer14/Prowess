20e136690418ed644b403f3252aa2f5063394e2e
Michael Ostrovsky <micha.ostrov@gmail.com>
2020-11-09 23:17:34 +0200

    Reformatting for better readability

--- src/publisher.py
+++ src/publisher.py
@@ -1,5 +1,6 @@
 import pika
 import os
+import asyncio
 
 RABBIT_HOST = os.environ.get("RABBIT_HOST", "fox.rmq.cloudamqp.com")
 RABBIT_USER = os.environ.get("RABBIT_USER", "ortmbosi")
@@ -10,8 +11,9 @@ parameters = pika.ConnectionParameters(
     credentials=credentials, host=RABBIT_HOST, virtual_host=RABBIT_USER)
 
 
-def publish(body):
+async def publish(body):
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.basic_publish(exchange='test', routing_key='lambda-riot-api-requests', body=body)
+    channel.basic_publish(
+        exchange='test', routing_key='lambda-riot-api-requests', body=body)
     connection.close()
