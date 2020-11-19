864fddca0a926f0da8e1a52bc47c7c31d936b561
Michael Ostrovsky <micha.ostrov@gmail.com>
2020-11-19 20:29:52 +0200

    Converting get puuid to do an api call to the consumer api

--- get_puuid.py
+++ get_puuid.py
@@ -8,15 +8,17 @@ def main(event, context):
     path_params = event.get('pathParameters', {})
     game_name = path_params.get("gameName")
     tag_line = path_params.get("tagLine")
-    r = redis.Redis(host=environ.get('REDIS_URL'), db=0)
+    r = redis.Redis(host=environ.get("REDIS_URL"), port=environ.get(
+        "REDIS_PORT"), password=environ.get("REDIS_PWD"), db=0)
     redis_puuid = r.get('{}#{}'.format(game_name, tag_line))
     if redis_puuid is not None:
         player_puuid = {"puuid": redis_puuid.decode('utf8')}
     else:
-        riot_handler = RiotHandler()
-        player_puuid = riot_handler.get_puuid(game_name, tag_line)
+        payload = {"game_name": game_name, "tag_line": tag_line}
+        player_puuid = requests.get(environ.get(
+            "CONSUMER_API_URL"), params=payload).json()
         r.set('{}#{}'.format(game_name, tag_line),
-              player_puuid.get('puuid', 'Unknown'))
+              player_puuid.get('puuid'))
     return {
         "statusCode": 200,
         "body": json.dumps(player_puuid)
