94ef74c17bb20f540c1235844ad1002cbbf01fe4
Michael Ostrovsky <micha.ostrov@gmail.com>
2020-11-20 15:24:22 +0200

    Minimizing get puuid

--- get_puuid.py
+++ get_puuid.py
@@ -1,5 +1,4 @@
 import json
-import redis
 import requests
 from os import environ
 
@@ -10,27 +9,19 @@ def main(event, context):
     path_params = event.get('pathParameters', {})
     game_name = path_params.get("gameName")
     tag_line = path_params.get("tagLine")
-    r = redis.Redis(host=environ.get("REDIS_URL"), port=environ.get(
-        "REDIS_PORT"), password=environ.get("REDIS_PWD"), db=0)
-    redis_puuid = r.get('{}#{}'.format(game_name, tag_line))
+    payload = {"game_name": game_name, "tag_line": tag_line}
     try:
-        if redis_puuid is not None:
-            player_puuid = {"puuid": redis_puuid.decode('utf8')}
-        else:
-            payload = {"game_name": game_name, "tag_line": tag_line}
-            player_puuid = requests.get(environ.get(
-                "CONSUMER_API_URL"), params=payload).json()
-            r.set('{}#{}'.format(game_name, tag_line),
-                  player_puuid.get('puuid'))
+        player_puuid = requests.get(environ.get(
+            "CONSUMER_API_URL") + "/api/v1/prowess/puuid", params=payload).text
         return {
             "statusCode": 200,
-            "body": json.dumps(player_puuid)
+            "body": player_puuid
         }
     except RuntimeError as e:
         logger.error(e)
         return {
             "statusCode": 500,
-            "body": json.dumps({"error": "some error occurred"})
+            "body": json.dumps({"error": "An error has occurred"})
         }
 
 
