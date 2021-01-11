f5cd0cf9b3939dca628a506c7f27e2089f6cb3cd
Michael Ostrovsky <micha.ostrov@gmail.com>
2021-01-12 00:20:39 +0200

    Refactoring code and adding playerInfo (rank)

--- src/match_analyzer.py
+++ src/match_analyzer.py
@@ -1,34 +1,43 @@
 from functools import reduce
 
+default_stats = {
+    "score": 0,
+    "roundsPlayed": 0,
+    "kills": 0,
+    "deaths": 0,
+    "assists": 0,
+    "playtimeMillis": 0,
+}
 
-def __extract_relevant_player(players_data, puuid):
+
+def _extract_relevant_player(players_data, puuid):
     relevant_player_list = [x for x in players_data if x.get('puuid') == puuid]
     if len(relevant_player_list) > 0:
         return relevant_player_list[0]
     return {}
 
 
-def __analyze_player_score(match, puuid):
+def _analyze_player_score(match, puuid):
     players_data = match.get('players', [])
-    extracted_player = __extract_relevant_player(players_data, puuid)
+    extracted_player = _extract_relevant_player(players_data, puuid)
     # Removing abilityCasts key as it is always null
     if extracted_player.get('stats') is not None:
-        extracted_player['stats'] = {key: extracted_player['stats'][key]
-                                     for key in extracted_player['stats'] if key != 'abilityCasts'}
-    return extracted_player.get('stats', {
-        "score": 0,
-        "roundsPlayed": 0,
-        "kills": 0,
-        "deaths": 0,
-        "assists": 0,
-        "playtimeMillis": 0,
-    })
+        extracted_player['stats'].pop(abilityCasts)
+        return extracted_player['stats']
+    return default_stats
 
 
-def __analyze_player_aim(match, puuid):
+def _get_player_info(match, puuid):
+    players_data = match.get('players', [])
+    extracted_player = _extract_relevant_player(players_data, puuid)
+    rank = extracted_player.get("competitiveTier")
+    return {"rank": rank, "characterId": extracted_player.get("characterId")}
+
+
+def _analyze_player_aim(match, puuid):
     def reduce_results(acc, val):
         players = val['playerStats']
-        relevant_player = __extract_relevant_player(players, puuid)
+        relevant_player = _extract_relevant_player(players, puuid)
         for damage_instance in relevant_player['damage']:
             acc["legshots"] += damage_instance["legshots"]
             acc["bodyshots"] += damage_instance["bodyshots"]
@@ -42,9 +51,9 @@ def __analyze_player_aim(match, puuid):
     })
 
 
-def __analyze_match_metadata(match, puuid):
+def _analyze_match_metadata(match, puuid):
     players_data = match.get('players', [])
-    relevant_player = __extract_relevant_player(players_data, puuid)
+    relevant_player = _extract_relevant_player(players_data, puuid)
     team_id = relevant_player.get('teamId')
     players_team = [x for x in match.get(
         'teams', []) if x['teamId'] == team_id]
@@ -61,10 +70,12 @@ def __analyze_match_metadata(match, puuid):
 
 
 def get_match_results(match, puuid):
-    match_overview_results = __analyze_player_score(match, puuid)
-    match_rounds_results = __analyze_player_aim(match, puuid)
-    match_metadata = __analyze_match_metadata(match, puuid)
+    match_overview_results = _analyze_player_score(match, puuid)
+    match_rounds_results = _analyze_player_aim(match, puuid)
+    match_metadata = _analyze_match_metadata(match, puuid)
+    player_metadata = _get_player_info(match, puuid)
     return {
         "data": {**match_overview_results, **match_rounds_results},
-        "currentMatchInfo": {**match_metadata}
+        "currentMatchInfo": {**match_metadata},
+        "playerInfo": {**player_metadata}
     }
