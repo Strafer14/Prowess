13d16dcfa30f8427bb4244a09d88c5b82d2d4e89
Michael Ostrovsky <micha.ostrov@gmail.com>
2021-01-12 00:19:04 +0200

    Renaming file

--- src/ValorantApi.py
+++ src/ValorantApi.py
@@ -1,11 +1,10 @@
 import requests
-import json
 from logger import logger
 from os import environ
 from ratelimit import limits, sleep_and_retry
 
 
-class RiotHandler:
+class ValorantApi:
     def __init__(self):
         token = environ.get('VALORANT_API_KEY')
         if token is None and environ.get("PYTHON_ENV") != 'testing':
@@ -26,11 +25,13 @@ class RiotHandler:
         return self.__make_request(url)
 
     def get_matches_list(self, region, puuid):
+        # noinspection SpellCheckingInspection
         url = 'https://{}.api.riotgames.com/val/match/v1/matchlists/by-puuid/{}'.format(
             region, puuid)
         return self.__make_request(url)
 
     def get_match_data(self, region, match_id):
+        # noinspection SpellCheckingInspection
         url = 'https://{}.api.riotgames.com/val/match/v1/matches/{}'.format(
             region, match_id)
         return self.__make_request(url)
