466182fd3282596158bba8768eeba81e90ea34b2
Michael Ostrovsky <micha.ostrov@gmail.com>
2021-09-15 19:26:06 +0300

    integrating coralogix

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,15 @@
 import logging
 from os import environ
 
+from coralogix.handlers import CoralogixLogger
 from discord_handler import DiscordHandler
 
-webhook_url = environ.get("DISCORD_WEBHOOK")
-agent = "Prowess"
+CORALOGIX_PRIVATE_KEY = environ.get("CORALOGIX_KEY")
+WEBHOOK_URL = environ.get("DISCORD_WEBHOOK")
+AGENT = "Prowess"
+SUB_SYSTEM = "api"
 
-logger = logging.getLogger(agent)
+logger = logging.getLogger(AGENT)
 logger.setLevel(logging.DEBUG)
 
 # Create formatter
@@ -14,11 +17,14 @@ logger.setLevel(logging.DEBUG)
 FORMAT = logging.Formatter(
     "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-if webhook_url is not None:
-    discord_handler = DiscordHandler(webhook_url, agent)
+if WEBHOOK_URL is not None:
+    discord_handler = DiscordHandler(WEBHOOK_URL, AGENT)
     discord_handler.setLevel(logging.INFO)
     discord_handler.setFormatter(FORMAT)
     logger.addHandler(discord_handler)
+if CORALOGIX_PRIVATE_KEY:
+    coralogix_handler = CoralogixLogger(CORALOGIX_PRIVATE_KEY, AGENT, SUB_SYSTEM)
+    logger.addHandler(coralogix_handler)
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
